6688e5a0c3df20090be1eb78600eeea5fcd52bdb
Maaz Khan <kmaaz8826@gmail.com>
2023-04-26 19:30:30 +0530

    fixed the problems

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,63 @@
 import './App.css';
-import react, {useState, useContext} from 'react'
+import react, {useState, createContext, useEffect} from 'react'
 import Cal from './Calender'
 
+export const userContext = createContext(null)
 let globalCounter = 0
-let habitCount = 0
+let days = []
+
+for(let i = 1; i<=30; i++){
+  days.push(i)
+}
 
 function App() {
-  const [cals,createCal] = useState([])
+  const defaultCalendars = localStorage.getItem("calendar")
+  const [cals,createCal] = useState(()=>{
+    return JSON.parse(localStorage.getItem('calendar')) || []
+  })
   const [habit,createHabit] = useState("")
+  const [crossed,setCrossed] = useState(false)
 
-  const deleteCalendar = (taskID) => {
+  useEffect(() => {localStorage.setItem("calendar", JSON.stringify(cals))},[cals])
+
+  const daysToPush = days.map(day => {
+    return {day: day, cross: crossed}
+  })
+
+  function crossDay(current,id){
+    cals.map(cal => {
+      if(cal.id===id){
+        cal.days.map(day =>{
+          if(day.day === current){
+              if(day.cross===false){
+                  return day.cross=true
+              } else {
+                  return day.cross=false
+              }
+          }
+      })
+      }
+    })
+    createCal([...cals])
+}
+
+  function deleteCalendar(taskID) {
+    console.log('I was run')
     createCal(cals.filter(item => item.id!==taskID))
+    console.log(cals)
+    console.log(taskID)
   }
 
   function createCalendar(event){
     event.preventDefault()
     createCal(items => {
       createHabit("")
-      return [...items,{id:globalCounter++,habits:habit}]
+      return [...items,{id:globalCounter++,habits:habit,days:[...daysToPush]}]
     })
-    console.log(globalCounter)
   }
 
   return (
-    <main className="bg-pink-500 h-[100vh] text-gray-900 flex flex-col place-items-center">
+    <main className="bg-pink-500 h-[100%] text-gray-900 flex flex-col place-items-center">
       <h1 className='p-4 pb-1 text-4xl text-white mb-8 font-bold'>Habit Tracking Calendars</h1>
       <form onSubmit={createCalendar} className='mb-8'>
         <input type='text' className='bg-gray-900 text-lg shadow-xl shadow-pink-800/50 hover:shadow-pink-800/80 mr-3 py-1 rounded text-white' value={habit} required onChange={(event) => createHabit(event.target.value)}></input>
@@ -32,7 +66,9 @@ function App() {
         <div>
           {cals.map(cal => {
             return <div className='text-black w-[40vw] flex flex-col place-items-center mb-10' key={cal.id}>
-              <Cal name={cal.habits} id={cal.id} delCal = {deleteCalendar} />
+              <userContext.Provider value={{cal,delCal:deleteCalendar, crossDay:crossDay}}>
+                <Cal />
+              </userContext.Provider>
             </div>
           })}
         </div>
--- src/Calender.jsx
+++ src/Calender.jsx
@@ -1,18 +1,20 @@
-import React from "react";
+import React, {useContext} from "react";
 import Days from "./Days";
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import {userContext} from './App.jsx'
 
-export default function Cal(props){
-    const {name,id,delCal} = props
+export default function Cal(){
+    const calData = useContext(userContext)
+    const {cal,delCal} = calData
+    const {id, habits, days} = cal
 
     return (
         <div className='bg-gray-100 rounded-lg w-full flex flex-col shadow-xl shadow-pink-900/50 place-items-center hover:shadow-pink-900/80'>
                 <h1 className="font-semibold text-4xl my-2 relative w-full text-center">
-                    {name}
+                    {habits}
                     <FontAwesomeIcon icon={faTrash} onClick={() => delCal(id)} className="text-pink-600 absolute top-1 right-4 text-xl [tab-index:1] cursor-pointer" />
                 </h1>
-                {/* <FontAwesomeIcon icon={faTrash} className="text-pink-600 absolute top-0 right-0" /> */}
            <div className="grid grid-cols-10 w-full place-items-center px-4 pb-3 pt-2">
             <Days />
            </div>
